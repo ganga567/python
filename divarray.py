f23798b8cc3e6a3a92fcbc7af9eff643ef689225
TADIBOYINA BALAGANGADHAR <139424283+ganga567@users.noreply.github.com>
2023-08-19 14:35:25 +0530

    Add files via upload

---
+++ divarray.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+zeroArray = np.array (100)
+zeroArray1 = np.array (200)
+resultArray = zeroArray % zeroArray1
+
+print("The resultArray Dimension array dimension is :", resultArray.ndim)
+print("The addition of two Zero arrays is :", resultArray)
+print("The Zero Dimension array is : ",zeroArray)
+print("The zero Dimension array dimension is :", zeroArray.ndim)
+print("The number of elements in zero Dimension array is : ", zeroArray.size)
+print(" The zero Dimension Array Shape is :" ,zeroArray.shape)
