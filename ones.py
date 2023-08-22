8f056bcc7d4107e621e8ff6f86cdad3b58c1e7d1
TADIBOYINA BALAGANGADHAR <139424283+ganga567@users.noreply.github.com>
2023-08-22 13:02:12 +0530

    Add files via upload

---
+++ ones.py
@@ -0,0 +1,10 @@
+import numpy as np
+twoDarray = np.array([[3,4],[8,9]])
+firstarray = np.ones((2,2),dtype=int)
+print("the oncs array is:\n",firstarray)
+addition_matrix1 = twoDarray + firstarray
+print("The addition matrix is:\n",addition_matrix1)
+subraction_matrix1 = twoDarray - firstarray
+print("The subraction matrix is:\n",subraction_matrix1)
+multiplication_matrix1 = twoDarray * firstarray
+print("The  multiplication matrix is:\n",multiplication_matrix1)
