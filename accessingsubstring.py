642b8029934241a554123d667c696b99502e9f42
TADIBOYINA BALAGANGADHAR <139424283+ganga567@users.noreply.github.com>
2023-12-18 17:41:02 +0530

    Add files via upload

---
+++ accessingsubstring.py
@@ -0,0 +1,14 @@
+# Create a string
+string = "Ekip It ,solutions"
+print("String:", string)
+
+# Concatenate strings
+additional_text = " Web Development"
+concatenated_string = string + additional_text
+print("Concatenated String:", concatenated_string)
+
+# Access substring
+substring_start = 0
+substring_end = 5
+substring = string[substring_start:substring_end]
+print("Substring:", substring)
