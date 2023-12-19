e0fcdca5b64823cf43d9a82325f4d175d76459d4
TADIBOYINA BALAGANGADHAR <139424283+ganga567@users.noreply.github.com>
2023-12-19 17:29:37 +0530

    Add files via upload

---
+++ arthimaticoperations.py
@@ -0,0 +1,24 @@
+"""Write a program to perform different Arithmetic Operations on numbers in Python. """
+num1 = int(5)
+num2 = int(10)
+'""addition'""
+add = num1 + num2
+'""subraction'""
+sub = num1 - num2
+'""multiplication'""
+mul = num1 * num2
+'""Division'""
+div = num1 / num2
+'""floor division'""
+floor_div = num1 // num2
+'""Exponent'""
+power = num1 ** num2
+'""modules'""
+modulus = num1 % num2
+print('Sum of ',num1+num2,'is')
+print('Sub of ',num1-num2,'is')
+print('Product of',num1*num2,'is')
+print('Division of ',num1/num2,'is')
+print('Floor Division of' ,num1//num2,'is')
+print('Exponent of ',num1**num2,'is')
+print('Modulus of ',num1%num2,'is')
