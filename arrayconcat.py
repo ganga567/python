e7d4a427e7b44586f4725b4832821da144bb58d0
TADIBOYINA BALAGANGADHAR <139424283+ganga567@users.noreply.github.com>
2024-01-12 21:36:27 +0530

    Add files via upload

--- arrayconcat.py
+++ arrayconcat.py
@@ -3,11 +3,16 @@ firstArray =np.ones([4,4],dtype=int)
 secondArray =np.zeros([4,2],dtype=int)
 concatArray = np.concatenate([firstArray,secondArray],axis=1)
 
-
 print ("The First Array is :",firstArray)
+
 print ("The First Array dimension is : ", firstArray .ndim)
+
 print ("The Second Array is :",secondArray)
+
 print ("The Second Array dimension is : ",secondArray.ndim)
-print ("The Concatinated array of first array and second array is :",concatArray)
-print ("The Concatinated array of first array and concat array is :",secondArray)
-print ("The Concatinated array of second array and concat array is :",firstArray)
+
+print ("The Concatinated array of first array and second array is :",concatArray.nidm)
+
+print ("The Concatinated array of first array and concat array is :",secondArray.nidm)
+
+print ("The Concatinated array of second array and concat array is :",firstArray.nidm)
